438a6ffda06520f4066056c2e1a2bec8da017bbc
Ashutosh Shanker <ashanker@snaproute.com>
2016-09-15 11:22:13 -0700

    Alarm unmarshalling

--- mail.go
+++ mail.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/gorilla/websocket"
@@ -10,6 +11,21 @@ import (
 	"os"
 	"os/signal"
 )
+
+type AlarmState struct {
+	OwnerId          int32  `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "Alarm owner daemon Id picked up from events.json"`
+	EventId          int32  `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "Alarm event id picked up from events.json"`
+	OwnerName        string `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "Alarm owner daemon name picked up from events.json"`
+	EventName        string `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "Alarm event name picked up from events.json"`
+	SrcObjName       string `SNAPROUTE: "KEY", ACCESS:"r", MULTIPLICITY:"*", DESCRIPTION: "Alarm event name picked up from events.json"`
+	Severity         string `DESCRIPTION: "Alarm Severity"`
+	Description      string `DESCRIPTION: "Description explaining the fault"`
+	OccuranceTime    string `DESCRIPTION: "Timestamp at which fault occured"`
+	SrcObjKey        string `DESCRIPTION: "Fault Object Key"`
+	SrcObjUUID       string `DESCRIPTION: "Fault Object UUID"`
+	ResolutionTime   string `DESCRIPTION: "Resolution Time stamp"`
+	ResolutionReason string `DESCRIPTION: "Cleared/Disabled"`
+}
 
 var addr = flag.String("addr", "localhost:8081", "http service address")
 
@@ -43,7 +59,10 @@ func main() {
 				log.Println("read:", err)
 				return
 			}
-			body := fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s\r\n\r\n%s", *to, "Alarm", message, *from)
+			var alarmState AlarmState
+			_ = json.Unmarshal(message, &alarmState)
+			alarmMsg := "OwnerName:" + alarmState.OwnerName + "\r\n" + "Event Name:" + alarmState.EventName + "\r\n" + "Object that caused Alarm:" + alarmState.SrcObjName + alarmState.SrcObjKey + "\r\n" + "UUID of the Object that caused Alarm:" + alarmState.SrcObjUUID + "\r\n" + "ResolutionTime:" + alarmState.ResolutionTime + "\r\n" + "ResolutionReason:" + alarmState.ResolutionReason
+			body := fmt.Sprintf("To: %s\r\nSubject: %s\r\n\r\n%s\r\n\r\n%s", *to, "Alarm", alarmMsg, *from)
 			auth := smtp.PlainAuth("", *from, *pwd, "smtp.gmail.com")
 			err = smtp.SendMail("smtp.gmail.com:587", auth, *from,
 				[]string{*to}, []byte(body))
